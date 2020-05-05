b741e6ae89daf6a333590783fb8187e1cd2928db
Romulo Eduardo <romuloedu@gmail.com>
2020-05-05 19:10:16 -0300

    Updates

---
+++ precache-manifest.5c865ef3728cad5055d58297a78fd003.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acc53cbc33356ba181509d571fb605cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65961f62343bddcc3e84",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "58f1e1553acf802bccc0",
+    "url": "/static/js/2.34d74ee6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.34d74ee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65961f62343bddcc3e84",
+    "url": "/static/js/main.82ef42af.chunk.js"
+  },
+  {
+    "revision": "a07415f3a612dc5038b4",
+    "url": "/static/js/runtime-main.2636bc25.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

818cded3ec6afcde6f255f9b44196e6661e7c6bf
Romulo Eduardo <romuloedu@gmail.com>
2020-05-06 10:56:46 -0300

    - Inclusão do texto da biografia..

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -9,11 +9,46 @@ const Bio = () => {
                 <h1>Sobre mim</h1>
             </div>
             <div className="bioDescription">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nu pariatur excepteur sint occaecat.</p>
+                <p>
+                    Formado em Análise e Desenvolvimento de Sistemas pela FAFICA,
+                    venho me especializando no desenvolvimento
+                    de aplicativos e sites utilizando a linguagem C# com os
+                    frameworks .NET Standard e .NET Core (multiplataforma).
+                </p>
+
+                <p>
+                    No contexto do desenvolvimento de softwares, trabalhei com
+                    ORMs como NHibernate e Entity Framework, as linguagens SQL
+                    e PL/SQL e com os bancos Oracle, MySQL, PostgreSQL e MongoDB.
+                    Além de padrões de projeto com MVC e MVVM.
+                </p>
+                <p>
+                    Para desenvolvimento front-end, trabalhei com frameworks
+                    como AngularJS, JQuery e Bootstrap.
+                </p>
+                <p>
+                    Enquanto Coordenador de TI, gerenciei uma equipe de 18
+                    colaboradores dividido entre desenvolvimento,
+                    infraestrutura e suporte. Aqui pude implementar
+                    tecnologias de versionamento de código (TFS e GIT) e
+                    boas práticas de gerenciamento (SCRUM), além de mediar
+                    conflitos e planejar estrategicamente os objetivos do
+                    setor.
+                </p>
+                <p>
+                    Atuei ainda no Parque Tecnológico Itaipu, onde pude
+                    trabalhar com WPF (XAML), desenvolvendo um software de
+                    análise de fasores para a equipe de engenheiros da usina
+                    hidrelétrica.
+                </p>
+                <p>
+                    Possuo ainda experiência com integração e entrega
+                    continua usando Jenkins, ferramentas de DevOps como
+                    Docker e Azure e algoritmos de inteligência artificial
+                    usando R e Python.
+                </p>
             </div>
-        </section>
+        </section >
     );
 }
 
